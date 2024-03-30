074a8567f2b58b26d7bde0bbafb802341385411b
DmitryKrylow <dima20038266@gmail.com>
2024-03-30 13:31:43 +0400

    update TA2.PY file: new struct duplicates removed

--- TA2.py
+++ TA2.py
@@ -1,52 +1,76 @@
 #64	10 01
-oper, f, s = input().split()
+
+###s0
+oper, f, s = input().split() #y1y2y3
 sost = "s0"
 err = "000"
-ans = 0
-print(sost)
-print(oper,f,s,err,sep='\n')
+ans = 0#y6
+###
 
-if(len(f) < 64 or len(s) < 64 or len(s) != len(f)):
-    err = "100"
-    print("ERROR CODE:",err)
-    print("SOST:",sost)
-    exit(0)
-if(len(f) > 64 or len(s) > 64):
-    err = "101"
-    print("ERROR CODE:", err)
-    print("SOST: ", sost)
-    exit(0)
-if f.count("0")+f.count("1") != len(f) or s.count("0")+s.count("1") != len(s):
-    err = "011"
-    print("ERROR CODE:", err)
-    print("SOST:", sost)
-    exit(0)
 
-if oper == "10":
-    if int (s,2) == 0:
-        err = "001"
-        print("ERROR CODE:", err)
-        print("SOST:", sost)
+def printEnv():
+    print("ERROR CODE:", err)#y7
+    print("S:", sost)#y8
+    print("FIRST OP:", f)#y9
+    print("SECOND OP:", s)#y10
+    print("OPERATION:", oper)#y11
+    print("ANSWER:", bin(int(ans))[2::].zfill(64))#y12
+    print()#y13
+
+
+def validate():
+    global err, f, s,sost #y14y15y16y17
+    if(len(f) < 64 or len(s) < 64 or len(s) != len(f)): ##X1
+        err = "001" #y18
+        printEnv()######
+        #####S0
         exit(0)
-    ans = int(f,2) / int(s,2)
-    print("ERROR CODE:", err)
-    print("SOST:", sost)
-    print("FIRST OP:", f)
-    print("SECOND OP:", s)
-    print("OPERATIONS:", oper)
-    print("ANSWER:", ans)
-    exit(0)
+        #####S0
+    if(len(f) > 64 or len(s) > 64):
+        err = "010"
+        printEnv()
+        exit(0)
+    if f.count('0') + f.count('1') != len(f) or s.count('0') + s.count('1') != len(s):
+        err = "011"
+        printEnv()
+        exit(0)
+
+def sim():
+    global err,ans, oper, sost
+    if oper == "10":
+        if int(s, 2) == 0:
+            err = "100"
+            printEnv()
+            exit(0)
+        ans = int(f, 2) / int(s, 2)
+        if(len(bin(int(ans))[2::]) > 64):
+            err = '101'
+            printEnv()
+            exit(0)
+        printEnv()
+        exit(0)
+    elif oper == "01":
+        ans = int(f, 2) * int(s, 2)
+        if (len(bin(ans)[2::]) > 64):
+            err = '101'
+            printEnv()
+            exit(0)
+        printEnv()
+        exit(0)
+    else:
+        err = "110"
+        printEnv()
+        exit(0)
+##S1
+printEnv()
+######
+
+####S2
+validate()
+#######
+sim()
 
 
-elif oper == "01":
-    ans = int(f,2) * int(s,2)
-    print("ERROR CODE:", err)
-    print("SOST:", sost)
-    print("FIRST OP:", f)
-    print("SECOND OP:", s)
-    print("OPERATIONS:", oper)
-    print("ANSWER:", ans)
-    exit(0)
 
 
 
